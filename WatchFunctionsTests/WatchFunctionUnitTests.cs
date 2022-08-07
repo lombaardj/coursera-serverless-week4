b497a1ebd353324cbe5785f950e4629d44ef6272
Jacques Lombaard <lombaardj@gmail.com>
2022-08-07 11:47:11 +0200

    Initial commit

---
+++ WatchFunctionsTests/WatchFunctionUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace WatchFunctionsTests
+{
+    public class WatchFunctionUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
